0fad6ffe86603d08f5241e393bf3f7f03d00882b
Marvin Bidzana <marvinbidzana@gmail.com>
2025-11-24 22:19:41 +0100

    feat(router): active le scroll automatique vers le haut lors de la navigation
    - Configuration de withInMemoryScrolling dans app.config.ts. - Activation de scrollPositionRestoration: 'top' pour une meilleure UX.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,7 +8,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top', // Force le scroll en haut à chaque navigation
+        anchorScrolling: 'enabled'        // Permet aussi de scroller vers les ancres (#section)
+      })
+    ), provideClientHydration(withEventReplay()),
     provideAnimations()
   ]
 };
